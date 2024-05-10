73335efe2a0646d70f30c8d715d433ffb5f8a10b
Johan Sebastian Navarro Duarte <johnavar@c3a1c6.42berlin.de>
2024-05-10 17:37:48 +0200

    in progress

--- src/world/refraction.c
+++ src/world/refraction.c
@@ -14,7 +14,7 @@ static bool	is_included(t_list *container, t_shape *obj)
 	return (false);
 }
 
-static void	remove_hit(t_list **container, t_shape *obj)
+static void	remove_obj(t_list **container, t_shape *obj)
 {
 	t_list	*tmp;
 	t_list	*prev;
@@ -29,7 +29,6 @@ static void	remove_hit(t_list **container, t_shape *obj)
 				prev->next = tmp->next;
 			else
 				*container = tmp->next;
-			free(tmp);
 			return ;
 		}
 		prev = tmp;
@@ -37,19 +36,25 @@ static void	remove_hit(t_list **container, t_shape *obj)
 	}
 }
 
-void	free_lst(t_list *container)
+static t_list	*new_lst(t_shape *shape)
 {
-	t_list	*tmp;
+	static t_list	pool[MAX_NODES + 1];
+	static size_t	index = 0;
+	t_list			*node;
 
-	if (!container)
-		return ;
-	while (container->next)
-	{
-		tmp = container;
-		container = container->next;
-		free(tmp);
-	}
-	free(container);
+	node = &pool[index++ & (MAX_NODES)];
+	node->content = shape;
+	node->next = NULL;
+	return (node);
+}
+
+static void	set_indices(t_list *container, double *n)
+{
+	if (!container) 
+		*n = 1;
+	else
+		*n = ((t_shape *)ft_lstlast(container)
+				->content)->material.refractive_index;
 }
 
 void	find_refractive_indices(t_comps *comps, t_hit *i, t_hit *xs)
@@ -60,25 +65,17 @@ void	find_refractive_indices(t_comps *comps, t_hit *i, t_hit *xs)
 	while (xs) 
 	{
 		if (i == xs) 
-		{
-			if (!container) 
-				comps->n1 = 1;
-			else
-				comps->n1 = ((t_shape *)ft_lstlast(container)->content)->material.refractive_index;
-		}
+			set_indices(container, &comps->n1);
 		if (xs && is_included(container, xs->obj))
-			remove_hit(&container, xs->obj);
+			remove_obj(&container, xs->obj);
 		else
-			ft_lstadd_back(&container, ft_lstnew(xs->obj));
+			ft_lstadd_back(&container, new_lst(xs->obj));
 		if (i == xs) 
 		{
-			if (!container)
-				comps->n2 = 1;
-			else
-				comps->n2 = ((t_shape *)ft_lstlast(container)->content)->material.refractive_index;
+			set_indices(container, &comps->n2);
 			break ;
 		}
+		printf("before next");
 		xs = xs->next;
 	}
-	free_lst(container);
 }
