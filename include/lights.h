b4796bb99291496514b8e1592ee797b62fd893f1
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-05-29 19:01:57 +0200

    Feat: working on soft_shadows, add a new is_shadowed, a area_light to create a new area light, intensity_at to fill the level of intensity if the light is in shadow or not..

--- include/lights.h
+++ include/lights.h
@@ -7,9 +7,15 @@
 
 typedef struct s_light
 {
-	t_point	position;
-	t_color	intensity;
-	bool	in_shadow;
+	t_point		position;
+	t_color		intensity;
+	double		global_intensity;
+	t_point		corner;
+	t_vector	uvec;
+	int			usteps;
+	t_vector	vvec;
+	int			vsteps;
+	int			samples;
 }	t_light;
 
 typedef struct s_eye_normal
@@ -18,6 +24,17 @@ typedef struct s_eye_normal
 	t_vector	normal_v;
 }	t_eye_normal;
 
+typedef struct s_area_light_params
+{
+	t_point		corner;
+	t_vector	full_uvec;
+	int			usteps;
+	t_vector	full_vvec;
+	int			vsteps;
+	t_color		intensity;
+}	t_area_light_params;
+
 t_light	new_light(t_point position, t_color intensity);
+t_light	area_light(t_area_light_params *lp);
 
 #endif
