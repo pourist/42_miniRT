bdf1ad94340598241b9510c65394b3600c0c1e23
Pouriya Pour Bahrami <ppour-ba@c4a11c10.42berlin.de>
2024-05-31 17:10:58 +0200

    31.May

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@ int	main(int argc, char **argv)
 
 	if (parser(argc, argv, &minirt))
 		return (1);
-	printf("r: %f g %f b: %f \n", minirt.world.ambient.r, minirt.world.ambient.g, minirt.world.ambient.b);
+	printf("Ambient:     r: %f g: %f b: %f \n", minirt.world.ambient.r, minirt.world.ambient.g, minirt.world.ambient.b);
+	printf("Light rgb:   r: %f g: %f b: %f \n", minirt.world.lights[0].intensity.r, minirt.world.lights[0].intensity.g, minirt.world.lights[0].intensity.b);
+	printf("Light pos:   x: %f y: %f z: %f \n", minirt.world.lights[0].position.x, minirt.world.lights[0].position.y, minirt.world.lights[0].position.z);
 	return (0);
 }
---
+++ src/parser/init_light.c
@@ -0,0 +1,45 @@
+#include "parser.h"
+
+
+// L -40.0,50.0,0.0  0.6 10,0,255
+// ∗ identifier: L
+// ∗ x,y,z coordinates of the light point: -40.0,50.0,0.0
+// ∗ the light brightness ratio in range [0.0,1.0]: 0.6
+// ∗ (unused in mandatory part)R,G,B colors in range [0-255]: 10, 0, 255
+
+void    create_light(char **rgb, double ratio, char **pos,t_light *light)
+{
+    double  r;
+    double  g;
+    double  b;
+
+    r = (ft_atof(rgb[0]) / 255) * ratio;
+    g = (ft_atof(rgb[1]) / 255) * ratio;
+    b = (ft_atof(rgb[2]) / 255) * ratio;
+    light->intensity = new_color(r, g, b);
+    light->position = new_point(ft_atof(pos[0]), ft_atof(pos[1]), ft_atof(pos[2]));
+    free_s(rgb);
+    free_s(pos);
+}
+
+int init_light(t_line_parse_env *env, t_light *light)
+{
+    char    **rgb;
+    char    **pos;
+
+    if (ft_strarr_len(env->line) != 4)
+        return (file_error(env, ERR_LIGHT));
+    env->error_type = L_RATIO;
+    if (solo(env->line[2], 0, 1, env))
+        return (1);
+    env->error_type = POS;
+    pos = ft_subsplit(env->line[1], ",\n");
+    if (triplets(pos, (double)INT_MIN, (double)INT_MAX, env))
+        return (1);
+    env->error_type = RGB;
+    rgb = ft_subsplit(env->line[3], ",\n");
+    if (triplets(rgb, 0, 255, env))
+        return (free_s(pos), 1);
+    create_light(rgb, ft_atof(env->line[2]), pos,light);
+    return (0);
+}
