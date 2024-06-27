5aaf90b2bf89766d68de8a0a28767543f4272c25
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2024-06-27 15:06:51 +0200

    Fix: cube parser correct width

--- src/parser/init_cube.c
+++ src/parser/init_cube.c
@@ -29,12 +29,12 @@ void    make_cube(t_cube_info *cube, char **center, char **axis, t_shape *obj)
     new_cube(obj);
     new_color(cube->r, cube->g, cube->b, &obj->material.color);
     new_vector(0, 1, 0, &default_axis);
+	new_vector(ft_atof(axis[0]), ft_atof(axis[1]), ft_atof(axis[2]), &axis_v);
+	calculate_rotation_matrix(&default_axis, &axis_v, &rot_m);
     translation(ft_atof(center[0]), ft_atof(center[1]), ft_atof(center[2]), &trans_m);
     scaling(cube->width, cube->height, cube->depth, &scale_m);
-    new_vector(ft_atof(axis[0]), ft_atof(axis[1]), ft_atof(axis[2]), &axis_v);
-    calculate_rotation_matrix(&default_axis, &axis_v, &rot_m);
     multiply_matrices(&rot_m, &trans_m, &final_m);
-    multiply_matrices(&scale_m, &final_m, &final_m);
+    multiply_matrices(&final_m, &scale_m, &final_m);
     set_transform(obj, &final_m);
 }
 
@@ -44,7 +44,7 @@ int cube_info(t_line_parse_env *env, t_cube_info *cube)
 
     cube->depth = ft_atof(env->line[5]);
     cube->height = ft_atof(env->line[4]);
-    cube->width = ft_atof(env->line[2]);
+    cube->width = ft_atof(env->line[3]);
     env->error_type = HEIGHT_VALID;
 	if (solo(env->line[4], EPSILON, (double)INT_MAX, env))
 		return (1);
