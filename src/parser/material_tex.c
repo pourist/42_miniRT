592d776b2dec16d0e4a5ddfcf667a707457c0df4
ppour-ba <ppour-ba@Piran.fritz.box>
2024-07-11 20:43:15 +0200

    texture

--- src/parser/material_tex.c
+++ src/parser/material_tex.c
@@ -3,18 +3,13 @@
 int mat_tex(t_material *material, int *index, t_line_parse_env	*env)
 {
     char *texture_path;
-    int length;
+   // int length;
 
 	(*index)++;
 	if (!env->line[*index] || env->line[*index][0] == '\n')
 		return (file_error_line(env->line_number, ERR_MAT));
     texture_path = env->line[*index];
-    length = ft_strlen(texture_path);
-    if (length >= 6 && texture_path[0] == '"' && 
-        texture_path[length - 1] == '"' &&
-            ft_strncmp(&texture_path[length - 5], ".png\"", 5) == 0) 
-        new_texture_map(&material->pattern, texture_path);
-    else
-        return (file_error_line(env->line_number, ERR_TEX_FORMAT));
+    // length = ft_strlen(texture_path);
+    new_texture_map(&material->pattern, texture_path);
     return (0);
 }
